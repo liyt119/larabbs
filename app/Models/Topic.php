087c93caaa941871ecdb86d31ca62c7350d0317c
liyt119 <891759142@qq.com>
2021-02-05 14:56:21 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,14 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    //一个话题属于一个分类和用户
+    public function category()
+    {
+      return $this->belongsTo(Category::class);
+    }
+    public function user()
+    {
+      return $this->belongsTo(User::class);
+    }
+
 }
