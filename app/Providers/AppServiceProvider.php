5fafc110bfd01ba9281fce713de0d11e002b48ef
liyt119 <891759142@qq.com>
2021-02-18 16:33:11 +0800

    翻译URL标识

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Providers;
+use App\Models\Topic;
+use App\Observers\TopicObserver;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Topic::observe(TopicObserver::class);
     }
 }
