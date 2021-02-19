c75c8186adf19fa3682424ac4e61952a36b98a52
liyt119 <891759142@qq.com>
2021-02-19 16:22:05 +0800

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -14,6 +15,9 @@ class ReplyObserver
         $topic= $reply->topic()->first(['id','reply_count']);
         $topic->reply_count =$topic->replies()->count();
         $topic->save();
+
+        //通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
     public function creating(Reply $reply)
